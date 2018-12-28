24f3a94ebade0c265af5d1aaa3da80bb96c9bded
marcusbass323 <marcus@bassconsultantgroup.com>
2018-12-27 20:32:18 -0800

    mailchimp API w/ options added

--- app.js
+++ app.js
@@ -23,7 +23,45 @@ app.post('/signup', (req, res) => {
             res.redirect('/success.html')
         }
 
+    //REQ DATA
 
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    }
+
+    const postData = JSON.stringify(data);
+
+    const options = {
+        url: 'https://us19.api.mailchimp.com/3.0/lists/7f11fd9236',
+        method: 'POST',
+        headers: {
+            Authorization: 'auth b1b344c28c087c71888788935f0b24af-us19'
+        },
+        body: postData
+    }
+
+    request(options, (err, response, body) => {
+        if (err) {
+            res.redirect('/fail.html')
+        } else {
+            if (response.statusCode === 200) {
+                res.redirect('/success.html')
+            } else {
+                res.redirect('/fail.html')
+            }
+        }
+    });
+
+})
 const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
